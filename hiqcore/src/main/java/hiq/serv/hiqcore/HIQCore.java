51d7f42d0be543f5694428431c64f7789daa75c6
YendisFish <44734948+YendisFish@users.noreply.github.com>
2021-10-01 22:56:15 -0600

    [NOT FINISHED]
    un-customizable version of the rules added

--- hiqcore/src/main/java/hiq/serv/hiqcore/HIQCore.java
+++ hiqcore/src/main/java/hiq/serv/hiqcore/HIQCore.java
@@ -1,16 +1,21 @@
 package hiq.serv.hiqcore;
 
+import hiq.serv.hiqcore.commands.rules;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class HIQCore extends JavaPlugin {
+public class HIQCore extends JavaPlugin implements Listener{
 
     //public String AllowedPlayers = this.getConfig().getString("allowed-players");
     //public String[] prmP = AllowedPlayers.split(",");
@@ -19,12 +24,17 @@ public class HIQCore extends JavaPlugin {
     public void onEnable() {
         System.out.println(ChatColor.GREEN + "HIQ-Core started!");
 
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
+
+        getServer().getPluginManager().registerEvents((Listener) this, this);
+        getCommand("rules").setExecutor(new rules());
+
         String[] alparray = allowedplayers().toArray(new String[0]);
 
         for(int i = 0; i < alparray.length; i++) {
             System.out.println("Allowed Player: " + alparray[i]);
         }
-
     }
 
     public List<String> allowedplayers() {
@@ -34,6 +44,16 @@ public class HIQCore extends JavaPlugin {
         String[] tolist = AllowedPlayers.split(",");
         List<String> prmP = Arrays.stream(tolist).collect(Collectors.toList());
         return prmP;
+    }
+
+    public List<String> blacklistjoinmsgplayers() {
+        getConfig().options().copyDefaults();
+
+        String playerstr = getConfig().getString("blacklist-join-msg");
+        String[] tolist = playerstr.split(",");
+        List<String> playerlist = Arrays.stream(tolist).collect(Collectors.toList());
+
+        return playerlist;
     }
 
     //commands
@@ -51,9 +71,36 @@ public class HIQCore extends JavaPlugin {
                 }
             }
         }
+        if(command.getName().equalsIgnoreCase("evolve")) {
+            if(sender instanceof Player) {
+                Player p = (Player) sender;
 
+                if(allowedplayers().contains(p.getUniqueId().toString())) {
+                    p.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "YOU HAVE EVOLVED!");
+                    try {
+                        Vector dir = p.getLocation().getDirection();
+                        p.setVelocity(dir.multiply(Integer.parseInt(args[0])));
+                        p.getNoDamageTicks();
+                    } catch (Exception ex) {
+                        p.sendMessage(ChatColor.RED + "Please type a valid number!");
+                    }
+                }
+            }
+        }
         return true;
     }
 
+    //Events that need access to the config
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        Player p = (Player) e.getPlayer();
+
+        if(!blacklistjoinmsgplayers().contains(p.getUniqueId().toString())) {
+            p.sendMessage(ChatColor.AQUA + "Have you joined our discord server? Join at https://discord.gg/jhwHswMYgz");
+        } else  {
+            System.out.println(p.getDisplayName() + "is blacklisted for onJoin message!");
+        }
+    }
 
 }
